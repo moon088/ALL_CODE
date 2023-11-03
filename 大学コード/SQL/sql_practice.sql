16d636e49e9db154f14ca23026c4a32c0ee15a12
moon088 <naoyabsktbl@gamil.com>
2023-11-03 17:27:04 +0900

    sql課題

---
+++ 大学コード/SQL/sql_practice.sql
@@ -0,0 +1,6 @@
+/*データベースシステムsql演習課題*/
+
+/*課題1*/
+select * from confederationcup where position = "GK" AND (name = "JEFFERSON" OR name = "Gianluigi BUFFON" OR name like "Eiji%" OR name like "Guillermo%" OR name like "Vincent%" OR name like "Martin%");
+
+
