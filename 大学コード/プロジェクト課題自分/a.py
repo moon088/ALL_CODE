8044a84628ab7e2b3ca9155de02c90eef3540ffc
moon088 <naoyabsktbl@gamil.com>
2023-11-10 19:41:04 +0900

    tensor

---
+++ 大学コード/プロジェクト課題自分/a.py
@@ -0,0 +1,10 @@
+# TensorFlow と tf.keras のインポート
+import tensorflow.keras as keras
+import tensorflow as tf
+from tensorflow import keras
+
+# ヘルパーライブラリのインポート
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
